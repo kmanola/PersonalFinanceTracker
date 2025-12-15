30ed4b4de309c6a3d01af91b2f182bf5477ce653
Kristoffer Manola <kristoffer.manola@gmail.com>
2025-12-15 10:50:11 +0100

    (feat): Add apply migrations logic (dev/prod)

--- PersonalFinanceTracker.AppHost/AppHost.cs
+++ PersonalFinanceTracker.AppHost/AppHost.cs
@@ -1,22 +1,26 @@
-using Aspire.Hosting;
-
 var builder = DistributedApplication.CreateBuilder(args);
 
-//TODO var ska jag lägga endpoint? kanske ingensans behövs, annars
+//var env = builder.AddAzureAppServiceEnvironment("my-environment");
+
+//TODO var ska jag lÃ¤gga endpoint? kanske ingensans behÃ¶vs, annars
 // https://learn.microsoft.com/en-us/azure/postgresql/flexible-server/connect-csharp
 var endpoint = builder.AddParameter("postgres-endpoint", secret: true);
 var userName = builder.AddParameter("postgres-username", secret: true);
 var password = builder.AddParameter("postgres-password", secret: true);
 
 var postgres = builder.AddAzurePostgresFlexibleServer("postgres")
-    .RunAsContainer()
     .WithPasswordAuthentication(userName, password);
 
 var postgresdb = postgres.AddDatabase("postgresdb");
 
-builder.AddProject<Projects.PersonalFinanceTracker_Api>("personalfinancetracker-api")
-       .WithReference(postgresdb)
-       .WaitFor(postgresdb);
+postgres.RunAsContainer(c => c.WithPgAdmin());
 
+var api = builder.AddProject<Projects.PersonalFinanceTracker_Api>("personalfinancetracker-api")
+       .WithReference(postgresdb);
+
+var migrations = builder.AddEfMigrate(api, postgresdb);
+
+api.WaitForCompletion(migrations);
+api.WithChildRelationship(migrations);
 
 builder.Build().Run();
---
+++ PersonalFinanceTracker.AppHost/Extensions/DbMigrations.cs
@@ -0,0 +1,127 @@
+﻿using System.Diagnostics;
+using Aspire.Hosting.Pipelines;
+using Microsoft.Extensions.Logging;
+#pragma warning disable ASPIREPIPELINES001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+
+
+public static class ExtMethods
+{
+    extension(IDistributedApplicationBuilder builder)
+    {
+        public IResourceBuilder<ExecutableResource> AddEfMigrate(IResourceBuilder<ProjectResource> app, IResourceBuilder<IResourceWithConnectionString> database)
+        {
+            var projectDirectory = Path.GetDirectoryName(app.Resource.GetProjectMetadata().ProjectPath)!;
+
+            var efmigrate = builder.AddExecutable(
+                            $"ef-migrate-{app.Resource.Name}",
+                            "dotnet",
+                            projectDirectory)
+                .WithArgs("ef")
+                .WithArgs("database")
+                .WithArgs("update")
+                .WithArgs("--no-build")
+                .WithArgs("--connection")
+                .WithArgs(database.Resource)
+                .WithEnvironment("DOTNET_ENVIRONMENT", "Development")
+                .WaitFor(database);
+
+
+            efmigrate.WithPipelineStepFactory(factoryContext =>
+            {
+                var step = new PipelineStep
+                {
+                    Name = $"ef-migration-bundle-{app.Resource.Name}",
+                    Tags = [WellKnownPipelineTags.BuildCompute],
+                    Action = async context =>
+                    {
+                        var psi = new ProcessStartInfo
+                        {
+                            FileName = "dotnet",
+                            RedirectStandardError = true,
+                            RedirectStandardOutput = true,
+                            WorkingDirectory = projectDirectory
+                        };
+                        // dotnet ef migrations bundle --self-contained -r linux-x64
+                        psi = psi.WithArgs("ef", "migrations", "bundle", "--self-contained", "-r", "linux-x64");
+
+                        await psi.ExecuteAsync(context.Logger, context.CancellationToken);
+                    }
+                };
+
+                return [step];
+            });
+
+            efmigrate.WithPipelineConfiguration(context =>
+            {
+                var appContainerBuildSteps = context.GetSteps(app.Resource, WellKnownPipelineTags.BuildCompute);
+
+                var migrationBundle = context.GetSteps(efmigrate.Resource, WellKnownPipelineTags.BuildCompute);
+
+                appContainerBuildSteps.DependsOn(migrationBundle);
+            });
+
+            return efmigrate;
+        }
+    }
+
+    extension(ProcessStartInfo psi)
+    {
+        public ProcessStartInfo WithArgs(params string[] args)
+        {
+            foreach (var arg in args)
+            {
+                psi.ArgumentList.Add(arg);
+            }
+            return psi;
+        }
+
+        // Exec with logs
+
+        public Task<int> ExecuteAsync(ILogger logger, CancellationToken cancellationToken = default)
+        {
+            var tcs = new TaskCompletionSource<int>();
+
+            var process = new Process
+            {
+                StartInfo = psi,
+                EnableRaisingEvents = true
+            };
+
+            process.OutputDataReceived += (sender, e) =>
+            {
+                if (e.Data != null)
+                {
+                    logger.LogDebug(e.Data);
+                }
+            };
+
+            process.ErrorDataReceived += (sender, e) =>
+            {
+                if (e.Data != null)
+                {
+                    logger.LogDebug(e.Data);
+                }
+            };
+
+            process.Exited += (sender, e) =>
+            {
+                tcs.SetResult(process.ExitCode);
+                process.Dispose();
+            };
+
+            process.Start();
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+
+            cancellationToken.Register(() =>
+            {
+                if (!process.HasExited)
+                {
+                    process.Kill();
+                }
+            });
+
+            return tcs.Task;
+        }
+    }
+}
