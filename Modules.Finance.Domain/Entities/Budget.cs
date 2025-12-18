f7b73af51b722df840830f4299a19f370bf2f475
Kristoffer Manola <kristoffer.manola@gmail.com>
2025-12-18 21:07:48 +0100

    (fix): Change budget to dateonly + migration

--- Modules.Finance.Domain/Entities/Budget.cs
+++ Modules.Finance.Domain/Entities/Budget.cs
@@ -5,7 +5,7 @@ public class Budget
     public int Id { get; set; }
     public required string Name { get; set; }
     public decimal Amount { get; set; }
-    public DateTime StartDate { get; set; }
-    public DateTime EndDate { get; set; }
+    public DateOnly StartDate { get; set; }
+    public DateOnly EndDate { get; set; }
     public string Description { get; set; } = string.Empty;
 }
---
+++ Modules.Finance.Infrastructure/Migrations/20251218200536_ChangeBudgetToDateOnly.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Modules.Finance.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeBudgetToDateOnly : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "StartDate",
+                table: "Budgets",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "EndDate",
+                table: "Budgets",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "StartDate",
+                table: "Budgets",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndDate",
+                table: "Budgets",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+        }
+    }
+}
